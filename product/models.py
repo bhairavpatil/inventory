8b0a779ba3207b5e8a53c332ce4f8b5f5a5cf5b3
Bhairav <bhairav.patil@gmail.com>
2021-08-26 01:34:26 +0530

    Email Test Commit

--- product/models.py
+++ product/models.py
@@ -4,13 +4,12 @@ from category.models import Category
 # Create your models here.
 
 class Product(models.Model):
-	"""For product categories"""
+	"""Model For product categories"""
 	name = models.CharField(max_length=100)
 	desc = models.TextField()
 	priceperitem = models.IntegerField(max_length=10)
 	quantity = models.IntegerField(max_length=10)
 	category = models.ForeignKey(Category,on_delete=models.CASCADE)
-
 
 	def __str__(self):
 		return self.name
